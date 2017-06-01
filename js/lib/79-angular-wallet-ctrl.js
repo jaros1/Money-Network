88dd415fde106f6383d5e1d91401c25f716b01c1
jaros1 <github@gofreerev.com>
2017-06-01 10:49:24 +0200

    Add wallet API #169 working on test wallet in wallet page. now calling wallet site with a session id

--- js/lib/79-angular-wallet-ctrl.js
+++ js/lib/79-angular-wallet-ctrl.js
@@ -5,10 +5,11 @@ angular.module('MoneyNetwork')
         var controller = 'WalletCtrl';
         console.log(controller + ' loaded');
 
+        var BITCOIN_ADDRESS_PATTERN = '1[a-km-zA-HJ-NP-Z1-9]{25,34}' ;
+
         self.new_wallet_url = $location.search()['new_wallet_site'] ; // redirect from a MoneyNetwork wallet site?
         var tested_wallet_url = null ; // last tested url
-
-        var bitcoin_address_pattern = '1[a-km-zA-HJ-NP-Z1-9]{25,34}' ;
+        var test_sessionid = MoneyNetworkHelper.generate_random_password(60, true).toLowerCase();
 
         function get_relative_url (url) {
             var pgm = controller + '.get_relative_url: ' ;
@@ -18,16 +19,40 @@ angular.module('MoneyNetwork')
                 // remove protocol (http, https etc)
                 url = url.substr(pos+3) ;
                 pos = url.indexOf('/') ;
-                // remove domain from url
+                // remove domain from url (127.0.0.1 or proxy server)
                 if (pos == -1) return null ; // cannot be a ZeroNet site URL
                 url = url.substr(pos) ;
                 return url ;
             }
-            if (url.substr(0,1) == '/') return url ;
-            if (url.match(new RegExp('^' + bitcoin_address_pattern + '$'))) return '/' + url ;
-            if (url.match(new RegExp('^' + bitcoin_address_pattern + '\/'))) return '/' + url ;
+            if (url.substr(0,1) == '/') return url ; // already a relative url ...
+            if (url.match(new RegExp('^' + BITCOIN_ADDRESS_PATTERN + '$'))) return '/' + url ; // bitcoin address
+            if (url.match(new RegExp('^' + BITCOIN_ADDRESS_PATTERN + '\/'))) return '/' + url ; // bitcoin address
+            if (url.match(/\.bit$/)) return '/' + url ; // .bit domain
+            if (url.match(/\.bit\//)) return '/' + url ; // .bit domain
             return null ;
         } // get_relative_url
+
+        // https://stackoverflow.com/a/10997390/11236
+        // https://stackoverflow.com/questions/1090948/change-url-parameters
+        function updateURLParameter(url, param, paramVal){
+            var newAdditionalURL = "";
+            var tempArray = url.split("?");
+            var baseURL = tempArray[0];
+            var additionalURL = tempArray[1];
+            var temp = "";
+            if (additionalURL) {
+                tempArray = additionalURL.split("&");
+                for (var i=0; i<tempArray.length; i++){
+                    if(tempArray[i].split('=')[0] != param){
+                        newAdditionalURL += temp + tempArray[i];
+                        temp = "&";
+                    }
+                }
+            }
+
+            var rows_txt = temp + "" + param + "=" + paramVal;
+            return baseURL + "?" + newAdditionalURL + rows_txt;
+        }
 
         self.show_test_new_wallet = function(context) {
             var pgm = controller + '.show_test_new_wallet: ' ;
@@ -47,108 +72,209 @@ angular.module('MoneyNetwork')
             tested_wallet_url = self.new_wallet_url ;
             run_tests() ;
         };
-
+        self.reset_test = function () {
+            var i ;
+            // stop any running tests
+            for (i=0 ; i<self.tests.length ; i++) {
+                if (self.tests[i].info.text == 'Running') {
+                    self.tests[i].info.test_failed = true ;
+                    self.test_feedback(self.tests[i], 'failed') ;
+                }
+            }
+            // test stopped
+            self.test_running = false ;
+            // reset test array
+            init_tests() ;
+            for (i=0 ; i<self.tests.length ; i++) {
+                self.tests[i].info.status = 'Pending' ;
+                delete self.tests[i].info.test_skipped ;
+                delete self.tests[i].info.test_ok ;
+                delete self.tests[i].info.test_failed ;
+                delete self.tests[i].info.disabled ;
+            }
+            // new sessionid
+            test_sessionid = MoneyNetworkHelper.generate_random_password(60, true).toLowerCase();
+        };
         var test1_open_url = (function () {
             var pgm = controller + '.test1: ' ;
             var info = {
                 no: 1,
                 text: 'Open wallet URL',
-                status: 'Start test'
-            }
-            function feedback () {
-                var relative_url ;
-                console.log(pgm + 'arguments = ' + JSON.stringify(arguments)) ;
-                if (arguments[0] == 'start test') {
-                    info.status = 'Running' ;
-                    relative_url = get_relative_url(self.new_wallet_url) ;
-                    ZeroFrame.cmd("wrapperOpenWindow", [relative_url, "_blank"]) ;
-                    return ;
+                status: 'Pending'
+            };
+            function run() {
+                var url, pos, search;
+                if (['Test skipped', 'Test OK'].indexOf(info.status) != -1) {
+                    // continue with next test
+                    console.log(pgm + 'test ' + info.no + ' done. start test ' + (info.no + 1));
+                    info.disabled = true;
+                    test2_allow_popup.run();
+                }
+                else {
+                    // start test 1
+                    info.status = 'Running';
+                    url = get_relative_url(self.new_wallet_url);
+                    url = updateURLParameter(url, 'sessionid', test_sessionid) ;
+                    ZeroFrame.cmd("wrapperOpenWindow", [url, "_blank"]);
                 }
             }
             return {
                 info: info,
-                feedback: feedback
+                run: run
             };
         })(); // test1
 
-        var test2_select_zeroid = (function () {
+        var test2_allow_popup = (function () {
             var pgm = controller + '.test2: ' ;
             var info = {
                 no: 2,
-                text: 'ZeroID selected in wallet?',
-                status: 'Not tested'
-            }
-            function feedback () {
-                console.log(pgm + 'arguments = ' + JSON.stringify(arguments)) ;
+                text: 'Popup allowed in browser',
+                status: 'Pending'
+            };
+            function run () {
+                var relative_url ;
+                if (['Test skipped', 'Test OK'].indexOf(info.status) != -1) {
+                    // continue with next test
+                    console.log(pgm + 'test ' + info.no + ' done. start test ' + (info.no+1)) ;
+                    info.disabled = true ;
+                    test3_select_zeroid.run() ;
+                }
+                else {
+                    // start test 2
+                    info.status = 'Running' ;
+                    ZeroFrame.cmd("wrapperConfirm", [info.text + '?'], function (ok) {
+                        if (!ok) return ;
+                        info.test_ok = true ;
+                        self.test_feedback(test2_allow_popup, 'OK') ;
+                        $rootScope.$apply() ;
+                    }) ;
+                }
             }
             return {
                 info: info,
-                feedback: feedback
+                run: run
             };
         })(); // test2
 
-        var test3_merger_moneynetwork = (function () {
-            var pgm = controller + '.test3: ' ;
-            var info = {
-                no: 3,
-                text: 'Merger:MoneyNetwork permission granted in wallet?',
-                status: 'Not tested'
-            }
-            function feedback () {
-                console.log(pgm + 'arguments = ' + JSON.stringify(arguments)) ;
-            }
-            return {
-                info: info,
-                feedback: feedback
-            };
-        })(); // test2
-
-        var test4_check_session = (function () {
+        var test3_select_zeroid = (function () {
             var pgm = controller + '.test2: ' ;
             var info = {
-                no: 4,
-                text: 'Checking session',
-                status: 'Not tested'
+                no: 3,
+                text: 'ZeroID selected in wallet',
+                status: 'Pending'
+            };
+            function run () {
+                var relative_url ;
+                if (['Test skipped', 'Test OK'].indexOf(info.status) != -1) {
+                    // continue with next test
+                    console.log(pgm + 'test ' + info.no + ' done. start test ' + (info.no+1)) ;
+                    info.disabled = true ;
+                    test4_merger_moneynetwork.run() ;
+                }
+                else {
+                    // start test 3
+                    info.status = 'Running' ;
+                    ZeroFrame.cmd('wrapperNotification', ['info', 'Please check that "ZeroId" is selected in wallet and click Test OK', 5000]);
+                }
             }
+            return {
+                info: info,
+                run: run
+            };
+        })(); // test3
+
+        var test4_merger_moneynetwork = (function () {
+            var pgm = controller + '.test3: ' ;
+            var info = {
+                no: 4,
+                text: 'Merger:MoneyNetwork permission granted in wallet',
+                status: 'Pending'
+            };
+            function run () {
+                var relative_url ;
+                if (['Test skipped', 'Test OK'].indexOf(info.status) != -1) {
+                    // continue with next test
+                    console.log(pgm + 'test ' + info.no + ' done. start test ' + (info.no+1)) ;
+                    info.disabled = true ;
+                    test5_check_session.run() ;
+                }
+                else {
+                    // start test 4
+                    info.status = 'Running' ;
+                    ZeroFrame.cmd('wrapperNotification', ['info', 'Please check that "Merger:MoneyNetwork permission" is granted in wallet and click Test OK', 5000]);
+                }
+            }
+            return {
+                info: info,
+                run: run
+            };
+        })(); // test4
+
+        var test5_check_session = (function () {
+            var pgm = controller + '.test2: ' ;
+            var info = {
+                no: 5,
+                text: 'Checking session',
+                status: 'Pending'
+            };
             function feedback () {
                 console.log(pgm + 'arguments = ' + JSON.stringify(arguments)) ;
+                return true ;
             }
             return {
                 info: info,
                 feedback: feedback
             };
-        })(); // test4
+        })(); // test5
 
         self.tests = [] ;
         function init_tests () {
             self.tests.splice(0, self.tests.length) ;
             self.tests.push(test1_open_url) ;
-            self.tests.push(test2_select_zeroid) ;
-            self.tests.push(test3_merger_moneynetwork) ;
-            self.tests.push(test4_check_session) ;
+            self.tests.push(test2_allow_popup) ;
+            self.tests.push(test3_select_zeroid) ;
+            self.tests.push(test4_merger_moneynetwork) ;
+            self.tests.push(test5_check_session) ;
         }
         init_tests();
         self.test_running = false ;
 
         function run_tests() {
-            var relative_url ;
             self.test_running = true ;
-            self.tests[0].feedback('start test') ;
+            self.tests[0].run() ;
             return ;
-            //status = 'Running' ;
-            //relative_url = get_relative_url(self.new_wallet_url) ;
-            //ZeroFrame.cmd("wrapperOpenWindow", [relative_url, "_blank"]) ;
         } // run_tests
 
+        self.test_feedback = function (test, checkbox) {
+            var checked ;
+            if (checkbox=='skipped') checked = test.info.test_skipped ;
+            if (checkbox=='OK') checked = test.info.test_ok ;
+            if (checkbox=='failed') checked = test.info.test_failed ;
 
-        self.test_feedback = function (test, result) {
-            if (result) {
-                test.status = 'Test OK' ;
-                test.test_failed = false ;
+            // checkbox feedback (skip, ok or failed)
+            if (checked) {
+                // check
+                test.info.old_status = test.info.status ; // save system status
+                test.info.status = 'Test ' + checkbox ;
+                if (checkbox == 'skipped') {
+                    test.info.test_ok = false ;
+                    test.info.test_failed = false ;
+                    if (test.info.old_status == 'Running') test.run() ; // next test
+                }
+                if (checkbox == 'OK') {
+                    test.info.test_skipped = false ;
+                    test.info.test_failed = false ;
+                    if (test.info.old_status == 'Running') test.run() ; // next test
+                }
+                if (checkbox == 'failed') {
+                    test.info.test_skipped = false ;
+                    test.info.test_ok = false ;
+                }
             }
             else {
-                test.status = 'Test failed' ;
-                test.test_ok = false ;
+                // uncheck
+                test.info.status = test.info.old_status ; // restore system status
+                delete test.info.old_status ;
             }
         };
 
