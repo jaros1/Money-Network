260ec0f8fdf3a2dd3aef2f6f7394ff4db245296b
jaros1 <github@gofreerev.com>
2017-06-01 18:06:52 +0200

    Add wallet API #169 first 5 steps wallet test OK. now ending with a fileGet to created wallet

--- js/lib/79-angular-wallet-ctrl.js
+++ js/lib/79-angular-wallet-ctrl.js
@@ -1,6 +1,8 @@
 angular.module('MoneyNetwork')
 
-    .controller('WalletCtrl', ['$rootScope', '$window', '$location', 'MoneyNetworkService', function ($rootScope, $window, $location, moneyNetworkService) {
+    .controller('WalletCtrl', ['$rootScope', '$window', '$location', '$timeout', 'MoneyNetworkService',
+                     function ($rootScope, $window, $location, $timeout, moneyNetworkService)
+    {
         var self = this;
         var controller = 'WalletCtrl';
         console.log(controller + ' loaded');
@@ -10,6 +12,7 @@ angular.module('MoneyNetwork')
         self.new_wallet_url = $location.search()['new_wallet_site'] ; // redirect from a MoneyNetwork wallet site?
         var tested_wallet_url = null ; // last tested url
         var test_sessionid = MoneyNetworkHelper.generate_random_password(60, true).toLowerCase();
+        var test_session_at = null ;
 
         function get_relative_url (url) {
             var pgm = controller + '.get_relative_url: ' ;
@@ -116,6 +119,7 @@ angular.module('MoneyNetwork')
                     url = get_relative_url(self.new_wallet_url);
                     url = updateURLParameter(url, 'sessionid', test_sessionid) ;
                     ZeroFrame.cmd("wrapperOpenWindow", [url, "_blank"]);
+                    test_session_at = new Date().getTime() ;
                 }
             }
             return {
@@ -157,7 +161,7 @@ angular.module('MoneyNetwork')
         })(); // test2
 
         var test3_select_zeroid = (function () {
-            var pgm = controller + '.test2: ' ;
+            var pgm = controller + '.test3: ' ;
             var info = {
                 no: 3,
                 text: 'ZeroID selected in wallet',
@@ -184,7 +188,7 @@ angular.module('MoneyNetwork')
         })(); // test3
 
         var test4_merger_moneynetwork = (function () {
-            var pgm = controller + '.test3: ' ;
+            var pgm = controller + '.test4: ' ;
             var info = {
                 no: 4,
                 text: 'Merger:MoneyNetwork permission granted in wallet',
@@ -211,19 +215,96 @@ angular.module('MoneyNetwork')
         })(); // test4
 
         var test5_check_session = (function () {
-            var pgm = controller + '.test2: ' ;
+            var pgm = controller + '.test5: ' ;
             var info = {
                 no: 5,
                 text: 'Checking session',
                 status: 'Pending'
             };
-            function feedback () {
-                console.log(pgm + 'arguments = ' + JSON.stringify(arguments)) ;
-                return true ;
-            }
+            function run () {
+                var relative_url, sessionid_sha256, query ;
+                if (['Test skipped', 'Test OK'].indexOf(info.status) != -1) {
+                    // test done
+                    info.disabled = true ;
+                    self.test_running = false ; ;
+                }
+                else {
+                    // start test 5. wait for wallet feedback
+                    info.status = 'Running' ;
+
+                    // wait for session to start. no event file done event. wait for db update. max 1 minute
+                    sessionid_sha256 = CryptoJS.SHA256(test_sessionid).toString();
+                    query = "" +
+                        "select json.directory " +
+                        "from keyvalue as keyvalue1, keyvalue as keyvalue2, json " +
+                        "where keyvalue1.key = 'sessionid_sha256' " +
+                        "and keyvalue1.value = '" + sessionid_sha256 + "' " +
+                        "and keyvalue2.json_id = keyvalue1.json_id " +
+                        "and keyvalue2.key = 'session_at' " +
+                        "and keyvalue2.value > '" + test_session_at + "' "  +
+                        "and json.json_id = keyvalue1.json_id" ;
+                    MoneyNetworkHelper.debug('select', 'query 18 = ' + query) ;
+                    var check_session = function(cb, count) {
+                        var debug_seq ;
+                        if (!count) count = 0;
+                        if (count > 60) return cb({ error: "timeout" }) ;
+                        debug_seq = MoneyNetworkHelper.debug_z_api_operation_start('z_db_query', pgm + 'query 18') ;
+                        ZeroFrame.cmd("dbQuery", [query], function (res) {
+                            var pgm = controller + '.test5.check_session dbQuery callback 1: ' ;
+                            var inner_path ;
+                            MoneyNetworkHelper.debug_z_api_operation_end(debug_seq) ;
+                            if (res.error) {
+                                console.log(pgm + 'Error when checking for new wallet session. error = ' + res.error) ;
+                                console.log(pgm + 'query = ' + query) ;
+                                return cb({ error: res.error }) ;
+                            }
+                            if (res.length == 0) {
+                                var job = function () { check_session(cb, count+1) };
+                                $timeout(job, 1000) ;
+                                return ;
+                            }
+                            // found updated wallet with correct sessionid_sha256.
+                            inner_path = 'merged-MoneyNetwork/' + res[0].directory + '/wallet.json' ;
+                            debug_seq = MoneyNetworkHelper.debug_z_api_operation_start('z_file_get', pgm + inner_path + ' fileGet') ;
+                            ZeroFrame.cmd("fileGet", {inner_path: inner_path, required: false}, function (wallet_str) {
+                                var pgm = controller + '.test5.check_session fileGet callback 2: ' ;
+                                MoneyNetworkHelper.debug_z_api_operation_end(debug_seq) ;
+                                if (!wallet_str) return (cb({ error: 'File ' + inner_path + ' was not found'})) ;
+                                cb(JSON.parse(wallet_str)) ;
+                            }) ; // fileGet callback 2
+                        }); // dbQuery callback 1
+                    }; // check_session
+                    check_session(function (res) {
+                        var elapsed ;
+                        if (res.error) {
+                            console.log(pgm + 'wallet session was not found. error = ' + res.error) ;
+                            info.status = 'Test failed' ;
+                            info.disabled = true ;
+                            self.test_running = false ;
+                        }
+                        else {
+                            elapsed = res.session_at - test_session_at ;
+                            console.log(pgm + 'new wallet session was found. pubkey2 = ' + res.pubkey2 + ', waited ' + Math.round(elapsed / 1000) + ' seconds') ;
+                            info.status = 'Test OK' ;
+                            info.disabled = true ;
+                            self.test_running = false ;
+                        }
+                        console.log(pgm + 'check_session. res = ' + JSON.stringify(res)) ;
+                        // res = {
+                        //    "sessionid_sha256": "2be2c9a124cfb85c307aa771d906c5c316f02ac22d61a43e1c6806b6c65c057a",
+                        //    "pubkey2": "Ahn94vCUvT+S/nefej83M02n/hP8Jvqc8KbxMtdSsT8R",
+                        //    "session_at": 1496332540922,
+                        //    "wallet_address": "1LqUnXPEgcS15UGwEgkbuTbKYZqAUwQ7L1",
+                        //    "wallet_title": "MoneyNetworkW2",
+                        //    "wallet_description": "Money Network - Wallet 2 - BitCoins www.blocktrail.com - runner jro"
+                        //};
+                        $rootScope.$apply() ;
+                    }) ; // check_session callback
+                }
+            } // run
             return {
                 info: info,
-                feedback: feedback
+                run: run
             };
         })(); // test5
 
