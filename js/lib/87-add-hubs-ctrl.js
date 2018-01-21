f34463265fea3fd093e819096d650036dc6b4d10
jaros1 <yje6P723Pf5pnsgpsY6ZLeJ4J3oC0I>
2018-01-21 08:51:41 +0100

    https://github.com/jaros1/Money-Network/issues/302#issuecomment-359189237 moved add/remove hub section to template + created a AddHubsCtrl controller

---
+++ js/lib/87-add-hubs-ctrl.js
@@ -0,0 +1,43 @@
+angular.module('MoneyNetwork')
+
+    // AddHubsCtrl. Used in addHubs template. add/remove user and wallet data hubs to/from MoneyNetwork merger site
+
+    .controller('AddHubsCtrl', ['$scope', 'safeApply',   'MoneyNetworkService',
+        function ($scope, safeApply,   moneyNetworkService) {
+
+        var self = this;
+        var controller = 'AddHubsCtrl';
+        console.log(controller + ' loaded');
+
+        // get a list of all user and wallet data hubs. For add hub site(s) UI
+        self.all_hubs = [] ;
+        moneyNetworkService.get_all_hubs(false, function (all_hubs) {
+            self.all_hubs = all_hubs ;
+            safeApply($scope) ;
+        }) ;
+
+        // add/remove data hubs.
+        self.add_remove_hub = function (hub_row) {
+            var pgm = controller + '.add_remove_hub: ' ;
+            if (hub_row.hub_added) {
+                ZeroFrame.cmd("mergerSiteAdd", [hub_row.hub], function (res) {
+                    console.log(pgm + 'mergerSiteAdd. res = ', JSON.stringify(res)) ;
+                    if (res == 'ok') hub_row.hub_added = true ;
+                }) ;
+            }
+            else {
+                ZeroFrame.cmd("mergerSiteDelete", [hub_row.hub], function (res) {
+                    console.log(pgm + 'mergerSiteDelete. res = ', JSON.stringify(res)) ;
+                    if (res == 'ok') hub_row.hub_added = false ;
+                }) ;
+            }
+        } ;
+        self.open_hub_url = function (hub_row) {
+            var pgm = controller + '.open_hub_url: ' ;
+            moneyNetworkService.open_window(pgm, hub_row.url) ;
+        };
+
+        // AddHubsCtrl
+    }])
+
+;
